2e5c5d3f7014f7a8f193c72bb71076d3c1db94e9
takeuchidaina <yda.takeuchidaina@gmail.com>
2020-07-03 11:26:00 +0900

    [update]ログ機能の追加
    スクロールバー,タイムスタンプ実装済み

--- Script/InputManager.cs
+++ Script/InputManager.cs
@@ -11,6 +11,18 @@ public class InputManager : MonoBehaviour
     {
         //InputFieldコンポーネントを取得
         inputField = GameObject.Find("InputField_Conversation").GetComponent<InputField>();
+
+        //入力待機状態にする
+        inputField.Select();
+    }
+
+    void Update()
+    {
+        //エンターキーが押されたら内容を送信する
+        if (Input.GetKeyDown(KeyCode.Return) && inputField.text != "")
+        {
+            GetInput();
+        }
     }
 
     //入力された内容を読み取ってコンソールに出力する関数
@@ -20,9 +32,16 @@ public class InputManager : MonoBehaviour
     {
         //InputFieldからテキスト情報を取得する
         string inputted = inputField.text;
-        Debug.Log(inputted);
+
+        //ログに入力内容を表示する
+        GameObject.Find("Text_Log").GetComponent<Log>().AddLog(inputted);
 
         //入力フォームのテキストを空にする
         inputField.text = "";
+
+        //入力待機状態にする
+        inputField.Select();
     }
+
+
 }
---
+++ Script/Log.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Log : MonoBehaviour
+{
+    public Text logs;   //ログが出力されるテキストボックス
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logs = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ログに文字列を追加する
+    public void AddLog(string _input)
+    {
+        logs.text += TimeStamp() + _input + "\n";
+    }
+
+    //タイムスタンプの文字列を返す
+    private string TimeStamp()
+    {
+        return "[" + DateTime.Now.ToLongTimeString() + "]　";
+    }
+}
