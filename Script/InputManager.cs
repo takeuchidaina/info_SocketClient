283c72825dfc86694b4f2d253499f87f0d34d5af
takeuchidaina <yda.takeuchidaina@gmail.com>
2020-07-03 09:58:12 +0900

    [update]インプットフィールドから値を取得するスクリプトの作成
    現状、エンターを押すとコンソールに表示

---
+++ Script/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    InputField inputField;      //InputFieldを格納
+
+    void Start()
+    {
+        //InputFieldコンポーネントを取得
+        inputField = GameObject.Find("InputField_Conversation").GetComponent<InputField>();
+    }
+
+    //入力された内容を読み取ってコンソールに出力する関数
+    //inspector上のOnEndEditにて選択
+    //Enterが押されると呼ばれる
+    public void GetInput()
+    {
+        //InputFieldからテキスト情報を取得する
+        string inputted = inputField.text;
+        Debug.Log(inputted);
+
+        //入力フォームのテキストを空にする
+        inputField.text = "";
+    }
+}
