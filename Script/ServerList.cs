17ca5c868a222ce6996cca6e2c99fe2afed1e81b
kuroneko96 <kaityou5555@gmail.com>
2020-08-04 14:06:55 +0900

    [add]サーバーリストの追加

---
+++ Script/ServerList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerList : MonoBehaviour
+{
+    private GameObject content;
+    private GameObject serverRoomPrefab;
+    private List<string> ipList = new List<string>();
+
+    private void Awake()
+    {
+        ipList.Add("20.00.1.17");
+        ipList.Add("20.00.10.4");
+        ipList.Add("20.00.10.24");
+        ipList.Add("20.00.12.15");
+        ipList.Add("20.01.1.7");
+
+        content = GameObject.Find("Content");
+        serverRoomPrefab = (GameObject)Resources.Load("ServerRoomNode");
+
+        Debug.Log(ipList.Count);
+    }
+
+    private void Start()
+    {
+        foreach(string ip in ipList)
+        {
+            Debug.Log(ip);
+            GameObject node = Instantiate(serverRoomPrefab, content.transform);
+            ServerRoomNode serverRoomNode = node.GetComponent<ServerRoomNode>();
+            serverRoomNode.ServerName = ip;
+            //serverRoomNode.SetServerIp(ip);
+        }
+    }
+}
---
+++ Script/ServerRoomNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerRoomNode : MonoBehaviour
+{
+    private string serverName = "20.00.10.24";
+
+    public string ServerName
+    {
+        get { return this.serverName; }
+        set { this.serverName = value; }
+    }
+
+    private void Start()
+    {
+        Text nameText = this.GetComponentInChildren<Text>();
+        nameText.text = serverName;
+    }
+}
