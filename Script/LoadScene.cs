6b1436bd9316f905ecc33831d2d8fb264a9bcb85
澤村遼 <ryo882514@gmail.com>
2020-08-18 13:11:34 +0900

    [add]シーンの追加

---
+++ Script/LoadScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    //それぞれに対応するボタンが押されたら
+    //それぞれのシーンに移動します
+
+    //サーバーリスト追加シーンに移動
+    public void AddServerButton()
+    {
+        SceneManager.LoadScene("AddServerScene");
+    }
+
+    //ダイレクト接続シーンに移動
+    public void DirectSendButton()
+    {
+        SceneManager.LoadScene("DirectSendScene");
+    }
+
+    //接続シーンに移動
+    public void SendButton()
+    {
+        SceneManager.LoadScene("SendScene");
+    }
+
+    //リモートクライアントシーンに移動
+    public void RemoteClient()
+    {
+        SceneManager.LoadScene("RemoteClient");
+    }
+
+}
