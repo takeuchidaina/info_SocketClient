51c789c6584c7fd98f7a5d8f944719687a6a61ae
dooooori <binamu1215@gmail.com>
2020-09-04 15:02:25 +0900

    [add]削除確認画面系追加

---
+++ Script/DeleteCheckText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteCheckText : MonoBehaviour
+{
+
+    private Text targetText;      //テキスト
+    private string ServerName;    //選択されたサーバー名の格納
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.targetText = this.GetComponent<Text>();    //テキストオブジェクト取得
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.targetText.text = "(選択されたサーバー)を\n本当に削除しますか？";
+    }
+}
