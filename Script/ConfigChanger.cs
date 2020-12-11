cc6e5408a902ee272156748373cf9914f6fae4cf
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-12-11 15:47:15 +0900

    [update]自動生成したUIで変更したデータを取得するためのプログラム

--- Script/ConfigChanger.cs
+++ Script/ConfigChanger.cs
@@ -20,8 +20,37 @@ public class ConfigChanger : MonoBehaviour
 
     public void ChangeConfig()
     {
-        //すべてのオブジェクトの値を格納するための
-        //Text[] myText = GameObject.Find(objName).GetComponentsInChildren<Text>();
+        //設定項目UIを格納する配列変数を宣言
+        GameObject[] configObject = new GameObject[jsonReader.MyTableCnt];
+
+        //設定項目UIを格納する
+        for(int i = 0; i < jsonReader.MyTableCnt; i++)
+        {
+            //NameListのUIを格納する
+            configObject[i] = GameObject.Find(jsonReader.NameList[i]);
+
+            //データのタイプによって処理を変える
+            switch (jsonReader.MyTableType[jsonReader.NameList[i]].type)
+            {
+                //Init型
+                case eType.Int:
+                    Debug.Log("おはよう");
+                    break;
+                //Double型
+                case eType.Double:
+                    Debug.Log("だぶる");
+                    break;
+                //String型
+                case eType.String:
+                    Debug.Log("すとりんぐ");
+                    break;
+                //Bool型
+                case eType.Bool:
+                    Debug.Log("ぶーる");
+                    break;
+            }
+        }
+
 
         for (int i = 0; i < jsonReader.MyTableCnt; i++)
         {
